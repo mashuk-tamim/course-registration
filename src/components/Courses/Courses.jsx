7599f4de503e545e86a0977ba62d2038df1ffea2
mashuk-tamim <tamimmashuk328@gmail.com>
2023-09-15 06:07:37 +0600

    header section completed and other components are made

---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Courses.propTypes = {
+    
+};
+
+function Courses(props) {
+    return (
+        <div>
+            <p>courses</p>
+        </div>
+    );
+}
+
+export default Courses;
