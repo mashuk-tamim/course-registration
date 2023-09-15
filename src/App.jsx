38cf8297fbe7dfabbf0c755e7d18a72884023449
mashuk-tamim <tamimmashuk328@gmail.com>
2023-09-15 08:09:34 +0600

    courses showing in the webpage responsively with required layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Courses from './components/Courses/Courses'
 import Header from './components/Header/Header'
@@ -5,14 +6,15 @@ import SelectedCourses from './components/SelectedCourses/SelectedCourses'
 
 function App() {
 
+
   return (
     <>
       <Header></Header>
-      <main>
-        <div>
+      <main className='w-11/12 mx-auto'>
+        <div className='md:w-3/4'>
           <Courses></Courses>
         </div>
-        <div>
+        <div className='w-1/4'>
           <SelectedCourses></SelectedCourses>
         </div>
       </main>
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,13 +1,24 @@
 import PropTypes from 'prop-types';
+import Course from './Course';
+import { useEffect, useState } from 'react';
 
 Courses.propTypes = {
     
 };
 
-function Courses(props) {
+function Courses() {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() =>{
+        fetch('../../../public/courses.json')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    },[])
     return (
-        <div>
-            <p>courses</p>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {
+                courses.map(course => <Course key={course.code} course={course}></Course>)
+            }
         </div>
     );
 }
